7738242d6e33451216a3854471a8446824090e39
rain <rcodmc@gmail.com>
2025-03-29 16:39:49 +0700

    [+] Added noCrystalBounce

--- src/main/java/me/twotwoqq/cmod/Main.java
+++ src/main/java/me/twotwoqq/cmod/Main.java
@@ -12,60 +12,71 @@ import java.util.HashMap;
 import java.util.UUID;
 
 public class Main implements ModInitializer {
+
     public static HashMap<UUID, Integer> playerPops = new HashMap<>();
-    public static HashMap<UUID, Integer> playerPing = new HashMap<>();
     public static boolean playerpopsToggle = true;
     public static boolean playerpingToggle = true;
     public static boolean nhcToggle = true;
     public static boolean nvToggle = true;
     public static boolean coToggle = true;
+    public static boolean nbToggle = true;
     public static MinecraftClient MC = MinecraftClient.getInstance();
+
+    private static final KeyBinding[] keyBindings = {
+            createKeyBinding("Toggle PopCounter", GLFW.GLFW_KEY_F7),
+            createKeyBinding("Toggle Ping", GLFW.GLFW_KEY_F6),
+            createKeyBinding("Reset PopCounter", GLFW.GLFW_KEY_F8),
+            createKeyBinding("Toggle NoHurtCam", GLFW.GLFW_KEY_8),
+            createKeyBinding("Toggle Name Visibility", GLFW.GLFW_KEY_9),
+            createKeyBinding("Toggle Crystal Optimizer", GLFW.GLFW_KEY_0),
+            createKeyBinding("Toggle noBounce", GLFW.GLFW_KEY_7)
+    };
+
+    private static final String[] messages = {
+            "Toggled PopCounter to ",
+            "Toggled Ping to ",
+            "PopCounter has been reset!",
+            "NoHurtCam is now ",
+            "Toggled Name Visibility to ",
+            "Toggled Crystal Optimizer to ",
+            "Toggled noBounce to "
+    };
+
+    private static final Runnable[] actions = {
+            () -> playerpopsToggle = !playerpopsToggle,
+            () -> playerpingToggle = !playerpingToggle,
+            () -> playerPops.clear(),
+            () -> nhcToggle = !nhcToggle,
+            () -> nvToggle = !nvToggle,
+            () -> coToggle = !coToggle,
+            () -> nbToggle = !nbToggle
+    };
 
     @Override
     public void onInitialize() {
-
-        KeyBinding toggle = KeyBindingHelper.registerKeyBinding(new KeyBinding("Toggle PopCounter",
-                GLFW.GLFW_KEY_F7,
-                "22qq Mod"));
-
-        KeyBinding pingtoggle = KeyBindingHelper.registerKeyBinding(new KeyBinding("Toggle Ping",
-                GLFW.GLFW_KEY_F6,
-                "22qq Mod"));
-
-        KeyBinding reset = KeyBindingHelper.registerKeyBinding(new KeyBinding("Reset PopCounter",
-                GLFW.GLFW_KEY_F8,
-                "22qq Mod"));
-        KeyBinding nhc = KeyBindingHelper.registerKeyBinding(new KeyBinding("Toggle NoHurtCam",
-                GLFW.GLFW_KEY_8,
-                "22qq Mod"));
-        KeyBinding nv = KeyBindingHelper.registerKeyBinding(new KeyBinding("Toggle Name Visibility",
-                GLFW.GLFW_KEY_9,
-                "22qq Mod"));
-        KeyBinding co = KeyBindingHelper.registerKeyBinding(new KeyBinding("Toggle Crystal Oprimizer",
-                GLFW.GLFW_KEY_0,
-                "22qq Mod"));
-
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (toggle.wasPressed()) {
-                playerpopsToggle = !playerpopsToggle;
-                MC.player.sendMessage(Text.of("§9[22qq's Custom Mod] §rToggled PopCounter to §e" + playerpopsToggle), false);
-            } else if (reset.wasPressed()) {
-                playerPops.clear();
-                MC.player.sendMessage(Text.of("§9[22qq's Custom Mod] §rPopCounter has been reset!"), false);
-            } else if (nhc.wasPressed()) {
-                nhcToggle = !nhcToggle;
-                client.player.sendMessage(Text.of("§9[22qq's Custom Mod] §rNoHurtCam is now §e" + nhcToggle), false);
-            } else if (pingtoggle.wasPressed()) {
-                playerpingToggle = !playerpingToggle;
-                client.player.sendMessage(Text.of("§9[22qq's Custom Mod] §rToggled Ping to §e" + playerpingToggle), false);
-            } else if (nv.wasPressed()) {
-                nvToggle = !nvToggle;
-                client.player.sendMessage(Text.of("§9[22qq's Custom Mod] §rToggled Name Visibility to §e" + nvToggle), false);
-            } else if (co.wasPressed()) {
-                coToggle = !coToggle;
-                client.player.sendMessage(Text.of("§9[22qq's Custom Mod] §rToggled Crystal Optimizer to §e" + coToggle), false);
+            for (int i = 0; i < keyBindings.length; i++) {
+                if (keyBindings[i].wasPressed()) {
+                    actions[i].run();
+                    client.player.sendMessage(Text.of("§9[22qq's Custom Mod] §r" + messages[i] + (i == 2 ? "" : "§e" + getToggleState(i))), false);
+                }
             }
         });
+    }
 
+    private static KeyBinding createKeyBinding(String name, int key) {
+        return KeyBindingHelper.registerKeyBinding(new KeyBinding(name, key, "22qq Mod"));
+    }
+
+    private static boolean getToggleState(int index) {
+        return switch (index) {
+            case 0 -> playerpopsToggle;
+            case 1 -> playerpingToggle;
+            case 3 -> nhcToggle;
+            case 4 -> nvToggle;
+            case 5 -> coToggle;
+            case 6 -> nbToggle;
+            default -> false;
+        };
     }
 }
